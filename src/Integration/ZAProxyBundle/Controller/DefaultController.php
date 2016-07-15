be07257017e9187431fb580ddbf9717d45297450
Sebastian Neubert <sebastian.neubert@bauerxcel.de>
2016-07-15 11:08:43 +0200

    update zaproxy security scanner bundle

--- src/Integration/ZAProxyBundle/Controller/DefaultController.php
+++ src/Integration/ZAProxyBundle/Controller/DefaultController.php
@@ -2,12 +2,37 @@
 
 namespace LeanKoala\Integration\ZAProxyBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+use Koalamon\Bundle\IncidentDashboardBundle\Entity\UserRole;
+use Koalamon\Bundle\IntegrationBundle\Controller\SystemAwareIntegrationController;
+
+class DefaultController extends SystemAwareIntegrationController
 {
+    const API_KEY = '98e3dcd5-3808-4a88-9abc-9ab9cbc3fddf';
+    const INTEGRATION_ID = 'ZAProxySecurityScanner';
+
+    protected function getIntegrationIdentifier()
+    {
+        return self::INTEGRATION_ID;
+    }
+
+    protected function getApiKey()
+    {
+        return self::API_KEY;
+    }
+
     public function indexAction()
     {
-        return $this->render('LeanKoalaIntegrationZAProxyBundle:Default:index.html.twig');
+        $this->assertUserRights(UserRole::ROLE_ADMIN);
+
+        return $this->render('LeanKoalaIntegrationZAProxyBundle:Default:index.html.twig',
+        [
+            'config' => $this->getConfig(),
+            'systems' => $this->getSystems(),
+            'integratedSystems' => $this->getIntegratedSystems(),
+            'optionsTemplate' => 'LeanKoalaIntegrationZAProxyBundle:Default:options.html.twig',
+            'optionsInTable' => false,
+            'storePath' => $this->generateUrl('leankoala_integration_zaproxy_store', ['project' => $this->getProject()->getIdentifier()])
+        ]);
     }
 }
